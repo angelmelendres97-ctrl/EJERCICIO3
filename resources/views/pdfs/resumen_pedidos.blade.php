825d95b49655c25d97f97adb217e6d4d49971e30
angelmelendres97-ctrl <angel.melendres97@gmail.com>
2026-01-08 13:30:36 -0500

    asd

--- resources/views/pdfs/resumen_pedidos.blade.php
+++ resources/views/pdfs/resumen_pedidos.blade.php
@@ -247,7 +247,7 @@
             </div>
 
             <div class="doc-line">
-                <span class="doc-number">N° {{ str_pad($resumen->codigo_secuencial, 8, '0', STR_PAD_LEFT) }}</span>
+                <span class="doc-number">O.C. N° {{ str_pad($resumen->codigo_secuencial, 8, '0', STR_PAD_LEFT) }}</span>
                 <span class="doc-type"> {{ $resumen->tipo }}</span>
             </div>
         </div>
